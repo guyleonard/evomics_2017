d9451913f54a57cc088660f9d0c503e5a8dd19a5
Guy Leonard <guy.leonard@gmail.com>
2016-08-23 11:35:07 +0100

    minor edits

--- base.sh
+++ base.sh
@@ -2,10 +2,6 @@
 set -x # debugging on
 set -e # exit if command exits with non-zero status
 
-## This script will do everything it can with apt-get and then switch to ansible
-# for other programs/software that need installing, intersperced with generic
-# unix utilities, git and possibly hombrew and conda.
-
 # Working Directory
 working_dir=$(pwd)
 
@@ -20,28 +16,9 @@ export ANSIBLE_NOCOWS=1
 
 # Clone the Evomics 2017 github repo
 git clone https://github.com/guyleonard/evomics_2017.git
-#
+
 # run the base playbook
 ansible-playbook /home/ubuntu/evomics_2017/base/base.yaml --sudo -K -c local -i "localhost,"
-
-## Java - We may need both openjdk and Oracle versions
-# default java - open java
-#apt-get -y install default-jre default-jdk
-# Oracle Java
-#add-apt-repository ppa:webupd8team/java
-#apt-get update
-#apt-get -y install oracle-java8-installer
-### Need to find an automated way to do this...
-#update-alternatives --config java
-
-# Development Software/Libraries
-
-
-
-
-
-
-
 
 
 # turn off
